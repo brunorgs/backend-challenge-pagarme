610669f86a3f2d2808e3c4701b94b22b8f752cd1
bruno <brunogonzaga10@hotmail.com>
2022-10-10 23:30:54 -0300

    initial project structure

---
+++ src/test/java/com/backend/challenge/pagarme/PagarmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.challenge.pagarme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagarmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
